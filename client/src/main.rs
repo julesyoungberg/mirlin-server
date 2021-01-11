815569c46e8301262def6d3d62e5776884b60e5b
Jules Youngberg <julesdayoungberg@gmail.com>
2021-01-10 21:34:06 -0800

    rust audio stream with spal

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,5 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::Data;
 use serde_json;
 use serde_json::{json, Value};
 use std::panic;
@@ -22,6 +24,7 @@ fn run() {
 
     let (mut receiver, mut sender) = client.split().unwrap();
 
+    // build subscription request
     let subscription_request = json!({
         "type": "subscription_request",
         "payload": {
@@ -29,8 +32,8 @@ fn run() {
         }
     });
 
+    // request subscription
     let request_message = OwnedMessage::Text(subscription_request.to_string());
-
     match sender.send_message(&request_message) {
         Ok(()) => (),
         Err(e) => {
@@ -39,6 +42,7 @@ fn run() {
         }
     }
 
+    // wait for a confirmation from the server
     let confirmation_msg = match receiver.recv_message() {
         Ok(msg) => msg,
         Err(e) => {
@@ -47,6 +51,7 @@ fn run() {
         }
     };
 
+    // the value must be serialized JSON, otherwise it is invalid
     let confirmation: Value = match confirmation_msg {
         OwnedMessage::Text(json_string) => serde_json::from_str(&json_string).unwrap(),
         _ => {
@@ -55,7 +60,45 @@ fn run() {
         }
     };
 
+    // successful confirmation
     println!("confirmation: {:?}", confirmation);
+
+    println!("configuring audio input device");
+
+    // get audio device
+    let host = cpal::default_host();
+    let device = host
+        .default_input_device()
+        .expect("no audio input device available");
+
+    // get supported config
+    let mut supported_configs_range = device
+        .supported_input_configs()
+        .expect("error while querying audio configs");
+    let supported_config = supported_configs_range
+        .next()
+        .expect("no supported audio config?!")
+        .with_max_sample_rate();
+
+    // create the stream
+    let stream = device
+        .build_input_stream(
+            &supported_config.config(),
+            move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                println!(
+                    "received {} bytes of audio input, forwarding to mirlin server",
+                    data.len()
+                );
+            },
+            move |err| {
+                println!("error: {:?}", err);
+            },
+        )
+        .unwrap();
+
+    stream.play().unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    drop(stream);
 
     // TODO begin sending audio data and receiving features
     // spin up 2 threads for this like here
