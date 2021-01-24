1e227437d62caf319fec0e0d4f1420f54170a423
Jules Youngberg <julesdayoungberg@gmail.com>
2021-01-24 13:32:24 -0800

    eNcApSuLaTiOn

---
+++ client/src/audio.rs
@@ -0,0 +1,164 @@
+use cpal;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use ringbuf::{Consumer, RingBuffer};
+use serde_json;
+use serde_json::{json, Value};
+use std::string::ToString;
+use std::thread;
+use websocket::client::ClientBuilder;
+use websocket::OwnedMessage;
+
+const CONNECTION: &'static str = "ws://127.0.0.1:9002";
+
+fn get_audio_device() -> cpal::Device {
+    let host = cpal::default_host();
+    host.default_input_device()
+        .expect("no audio input device available")
+}
+
+fn get_mic_config(device: &cpal::Device) -> cpal::SupportedStreamConfig {
+    // get supported config
+    let mut supported_configs_range = device
+        .supported_input_configs()
+        .expect("error while querying audio configs");
+    supported_configs_range
+        .next()
+        .expect("no supported audio config?!")
+        .with_max_sample_rate()
+}
+
+pub struct AudioFeatures {
+    pub consumer: Consumer<serde_json::Value>,
+    pub current: serde_json::Value,
+    pub recv_thread: std::thread::JoinHandle<()>,
+    pub stream: cpal::Stream,
+}
+
+impl AudioFeatures {
+    pub fn new() -> Self {
+        println!("configuring audio input device");
+        let audio_device = get_audio_device();
+        let mic_config = get_mic_config(&audio_device);
+        let cpal::SampleRate(sample_rate) = mic_config.sample_rate();
+        println!("sample_rate: {:?}", sample_rate);
+
+        println!("Connecting to {}", CONNECTION);
+        let client = ClientBuilder::new(CONNECTION)
+            .unwrap()
+            .add_protocol("rust-websocket")
+            .connect_insecure()
+            .unwrap();
+        let (mut receiver, mut sender) = client.split().unwrap();
+
+        // build subscription request
+        let session_request = json!({
+            "type": "session_request",
+            "payload": {
+                "features": [
+                    "rms",
+                    "energy",
+                    "centroid",
+                    "loudness",
+                    "noisiness",
+                    "pitch",
+                    "mfcc",
+                    "dissonance",
+                    "key",
+                    "tristimulus",
+                    "spectral_contrast",
+                    "spectral_complexity",
+                    "onset",
+                ],
+                "sample_rate": sample_rate,
+                "hop_size": 512 as u32, // happens to be cpal's buffer size
+                "memory": 4 as u32, // rember 4 frames including current
+            }
+        });
+        println!("Requesting subscription {:?}", session_request);
+        // request subscription
+        let request_message = OwnedMessage::Text(session_request.to_string());
+        match sender.send_message(&request_message) {
+            Ok(()) => (),
+            Err(e) => panic!("Error requesting subscription: {:?}", e),
+        }
+
+        // wait for a confirmation from the server
+        println!("Confirming subscription");
+        let confirmation_msg = match receiver.recv_message() {
+            Ok(msg) => msg,
+            Err(e) => panic!("Error confirming subscription: {:?}", e),
+        };
+        // the value must be serialized JSON, otherwise it is invalid
+        let confirmation: Value = match confirmation_msg {
+            OwnedMessage::Text(json_string) => serde_json::from_str(&json_string).unwrap(),
+            _ => panic!("Invalid confirmation message"),
+        };
+        println!("confirmation received: {:?}", confirmation);
+
+        // create the stream
+        let stream = audio_device
+            .build_input_stream(
+                &mic_config.config(),
+                move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                    let frame_message = json!({
+                        "type": "audio_frame",
+                        "payload": data,
+                    });
+                    // send frame to server
+                    let message = OwnedMessage::Text(frame_message.to_string());
+                    match sender.send_message(&message) {
+                        Ok(()) => (),
+                        Err(e) => {
+                            println!("Error sending frame");
+                            panic!(e);
+                        }
+                    }
+                },
+                move |err| {
+                    panic!(err);
+                },
+            )
+            .unwrap();
+        stream.play().unwrap();
+
+        // create a ring buffer for server responses (features)
+        let ring_buffer = RingBuffer::<serde_json::Value>::new(2);
+        let (mut producer, consumer) = ring_buffer.split();
+        producer.push(json!(null)).unwrap();
+        // listen for messages from server, push to ring buffer
+        let recv_thread = thread::spawn(move || {
+            for raw in receiver.incoming_messages() {
+                let message = match raw {
+                    Ok(m) => m,
+                    Err(e) => {
+                        println!("Error receiving message: {:?}", e);
+                        return;
+                    }
+                };
+                let value: Value = match message {
+                    OwnedMessage::Text(json_string) => serde_json::from_str(&json_string).unwrap(),
+                    _ => {
+                        println!("Received unexpected message: {:?}", message);
+                        return;
+                    }
+                };
+                producer.push(value).ok();
+            }
+        });
+
+        Self {
+            consumer,
+            current: json!(null),
+            recv_thread,
+            stream,
+        }
+    }
+
+    // update current value if new data is available
+    pub fn update(&mut self) {
+        let value = self.consumer.pop();
+        if !value.is_none() {
+            self.current = value.unwrap();
+        }
+    }
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,166 +1,23 @@
-use cpal;
-use cpal::traits::{DeviceTrait, StreamTrait};
 use nannou::prelude::*;
-use ringbuf::{Consumer, RingBuffer};
-use serde_json;
-use serde_json::{json, Value};
-use std::string::ToString;
-use std::thread;
-use websocket::client::ClientBuilder;
-use websocket::OwnedMessage;
 
-mod util;
-
-const CONNECTION: &'static str = "ws://127.0.0.1:9002";
+mod audio;
 
 fn main() {
     nannou::app(model).update(update).simple_window(view).run();
 }
 
-#[allow(dead_code)]
 struct Model {
-    consumer: Consumer<serde_json::Value>,
-    current: serde_json::Value,
-    recv_thread: std::thread::JoinHandle<()>,
-    stream: cpal::Stream,
+    audio_features: audio::AudioFeatures,
 }
 
 fn model(_app: &App) -> Model {
-    println!("configuring audio input device");
-    let audio_device = util::get_audio_device();
-    let mic_config = util::get_mic_config(&audio_device);
-    let cpal::SampleRate(sample_rate) = mic_config.sample_rate();
-    println!("sample_rate: {:?}", sample_rate);
-
-    println!("Connecting to {}", CONNECTION);
-
-    let client = ClientBuilder::new(CONNECTION)
-        .unwrap()
-        .add_protocol("rust-websocket")
-        .connect_insecure()
-        .unwrap();
-
-    let (mut receiver, mut sender) = client.split().unwrap();
-
-    // build subscription request
-    let session_request = json!({
-        "type": "session_request",
-        "payload": {
-            "features": [
-                "rms",
-                "energy",
-                "centroid",
-                "loudness",
-                "noisiness",
-                "pitch",
-                "mfcc",
-                "dissonance",
-                "key",
-                "tristimulus",
-                "spectral_contrast",
-                "spectral_complexity",
-                "onset",
-            ],
-            "sample_rate": sample_rate,
-            "hop_size": 512 as u32, // happens to be cpal's buffer size
-            "memory": 4 as u32, // rember 4 frames including current
-        }
-    });
-
-    println!("Requesting subscription {:?}", session_request);
-
-    // request subscription
-    let request_message = OwnedMessage::Text(session_request.to_string());
-    match sender.send_message(&request_message) {
-        Ok(()) => (),
-        Err(e) => panic!("Error requesting subscription: {:?}", e),
-    }
-
-    // wait for a confirmation from the server
-    println!("Confirming subscription");
-    let confirmation_msg = match receiver.recv_message() {
-        Ok(msg) => msg,
-        Err(e) => panic!("Error confirming subscription: {:?}", e),
-    };
-
-    // the value must be serialized JSON, otherwise it is invalid
-    let confirmation: Value = match confirmation_msg {
-        OwnedMessage::Text(json_string) => serde_json::from_str(&json_string).unwrap(),
-        _ => panic!("Invalid confirmation message"),
-    };
-
-    // successful confirmation
-    println!("confirmation received: {:?}", confirmation);
-
-    // create the stream
-    let stream = audio_device
-        .build_input_stream(
-            &mic_config.config(),
-            move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                let frame_message = json!({
-                    "type": "audio_frame",
-                    "payload": data,
-                });
-
-                // send frame to server
-                let message = OwnedMessage::Text(frame_message.to_string());
-                match sender.send_message(&message) {
-                    Ok(()) => (),
-                    Err(e) => {
-                        println!("Error sending frame");
-                        panic!(e);
-                    }
-                }
-            },
-            move |err| {
-                panic!(err);
-            },
-        )
-        .unwrap();
-
-    stream.play().unwrap();
-
-    let ring_buffer = RingBuffer::<serde_json::Value>::new(2); // Add some latency
-    let (mut producer, consumer) = ring_buffer.split();
-    producer.push(json!(null)).unwrap();
-
-    // listen for messages from server, push to ring buffer
-    let recv_thread = thread::spawn(move || {
-        for raw in receiver.incoming_messages() {
-            let message = match raw {
-                Ok(m) => m,
-                Err(e) => {
-                    println!("Error receiving message: {:?}", e);
-                    return;
-                }
-            };
-
-            let value: Value = match message {
-                OwnedMessage::Text(json_string) => serde_json::from_str(&json_string).unwrap(),
-                _ => {
-                    println!("Received unexpected message: {:?}", message);
-                    return;
-                }
-            };
-
-            producer.push(value).ok();
-        }
-    });
-
     Model {
-        consumer,
-        current: json!(null),
-        recv_thread,
-        stream,
+        audio_features: audio::AudioFeatures::new(),
     }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    // update current value if new data is available
-    let value = model.consumer.pop();
-    if !value.is_none() {
-        model.current = value.unwrap();
-    }
+    model.audio_features.update();
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -170,9 +27,9 @@ fn view(app: &App, model: &Model, frame: Frame) {
     // Clear the background to purple.
     draw.background().color(PLUM);
 
-    println!("current features: {:?}", model.current);
+    println!("current features: {:?}", model.audio_features.current);
 
-    if let Some(payload) = model.current.get("payload") {
+    if let Some(payload) = model.audio_features.current.get("payload") {
         if let Some(features) = payload.get("features") {
             let tristimulus = features
                 .get("tristimulus.mean")
